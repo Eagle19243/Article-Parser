4b8c1053a2ffb4a295335bf00c2021073871376d
Eagle19243 <eagle19243@gmail.com>
2019-04-16 10:48:15 -0400

    fix a couple of bugs and changed behavior to reflects reqs

--- article_parser/article_parser.py
+++ article_parser/article_parser.py
@@ -8,12 +8,12 @@ class ImageParser(HTMLParser):
         self.result = ""
         self.looking_for_caption = False
         self.open_caption = False
-        self.caption_data = ""
         self.last_pos = 0
+        self.depth = 0
         self.line_offset = [0, 0]
         for i, c in enumerate(self.body):
             if c == '\n':
-                self.line_offset.append(i)
+                self.line_offset.append(i + 1)  # + 1 to skip the '\n' char
         super().__init__()
 
     def run(self):
@@ -25,7 +25,6 @@ class ImageParser(HTMLParser):
         if tag == 'img':
             self.looking_for_caption = True
             self.open_caption = False
-            self.caption_data = ""
             alt = None
             line, offset = self.getpos()
             current_pos = self.line_offset[line] + offset
@@ -44,27 +43,29 @@ class ImageParser(HTMLParser):
                     alt, src
                 )
             )
-        elif tag == 'p':
-            if self.looking_for_caption:
-                self.looking_for_caption = False
-                self.open_caption = True
-        else:
-            if self.looking_for_caption and not self.open_caption:
-                self.looking_for_caption = False
+        elif self.looking_for_caption:
+            self.looking_for_caption = False
+            self.open_caption = True
+            self.depth = 1
+        elif self.open_caption:
+            self.depth += 1
 
     def handle_endtag(self, tag):
-        if tag == 'p':
-            if self.open_caption:
+        if self.looking_for_caption:
+            self.result += '</{}>'.format(tag)
+            self.last_pos += len(tag) + 3
+        if self.open_caption:
+            self.depth -= 1
+            if self.depth == 0:
                 line, offset = self.getpos()
                 current_pos = self.line_offset[line] + offset
-                self.last_pos = current_pos + 4 + 1  # len('</p>') == 4
-                caption = '<p class="caption">{}</p>'.format(self.caption_data)
-                self.result = self.result.format(caption=caption)
+                self.last_pos = current_pos + len(tag) + 3
                 self.open_caption = False
 
     def handle_data(self, data):
         if self.open_caption:
-            self.caption_data += data
+            caption = '<p class="caption">{}</p>'.format(data)
+            self.result = self.result.format(caption=caption)
 
 
 def parse_one(body):
@@ -84,7 +85,7 @@ def parse(file_path):
 def transform(input_file_path, output_file_path):
     with open(output_file_path, 'w') as foutput:
         data = parse(input_file_path)
-        json.dump(data, foutput, indent=4)
+        json.dump(data, foutput, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
--- article_parser/tests/test.py
+++ article_parser/tests/test.py
@@ -1,12 +1,65 @@
 import json
 import unittest
-from article_parser import transform
+from article_parser import transform, parse_one
+
 
 class Tester(unittest.TestCase):
-    def test(self):
+    def test_json(self):
         transform('tests/input.json', 'tests/output.json')
         with open('tests/output.json', 'r') as foutput:
             with open('tests/expected.json', 'r') as fexpected:
                 data = json.load(foutput)
                 expected = json.load(fexpected)
                 assert data == expected
+
+    def test_regular(self):
+        txt = (
+            'placeholder text at the beginning'
+            '<img a="1" b="2" alt="placeholder" src="#">'
+            '<p>This is the caption</p>'
+        )
+        ans = (
+            'placeholder text at the beginning'
+            '<div class="image"><img alt="placeholder" src="#"/>'
+            '<p class="caption">This is the caption</p></div>'
+        )
+        assert parse_one(txt) == ans
+
+    def test_img_wrapped(self):
+        txt = (
+            'placeholder text at the beginning'
+            '<div class="test"><img a="1" b="2" alt="placeholder" src="#"></div>'
+            '<p>This is the caption</p>'
+        )
+        ans = (
+            'placeholder text at the beginning'
+            '<div class="test"><div class="image"><img alt="placeholder" src="#"/>'
+            '<p class="caption">This is the caption</p></div></div>'
+        )
+        assert parse_one(txt) == ans
+
+    def test_caption_wrapped(self):
+        txt = (
+            'placeholder text at the beginning'
+            '<div class="test"><img a="1" b="2" alt="placeholder" src="#"></div>'
+            '<em><p>This is the caption</p></em>'
+        )
+        ans = (
+            'placeholder text at the beginning'
+            '<div class="test"><div class="image"><img alt="placeholder" src="#"/>'
+            '<p class="caption">This is the caption</p></div></div>'
+        )
+        assert parse_one(txt) == ans
+
+    def test_multiline(self):
+        txt = (
+            '<a></a>\n<a></a>\n<p>Some text....</p>\n'
+            '<div class="test"><img a="1" b="2" alt="placeholder" src="#"></div>'
+            '\n\n\n<em><p>This is the caption</p></em>'
+        )
+        ans = (
+            '<a></a>\n<a></a>\n<p>Some text....</p>\n'
+            '<div class="test"><div class="image"><img alt="placeholder" src="#"/>'
+            '<p class="caption">This is the caption</p></div></div>'
+        )
+        assert parse_one(txt) == ans
